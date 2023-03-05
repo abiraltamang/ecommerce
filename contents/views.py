7464892ad5be3dade80a6c440fca26de981232ab
abiraltamang <abiral.tamang816@gmail.com>
2023-03-05 12:13:08 +0545

    index

--- contents/views.py
+++ contents/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from products.models import Product
+from contents.models import Content
 
 def show_home(request):
-    return render(request,'index.html')
+    latest_products = Product.objects.order_by('-id')[:2]
+    sliders = Content.objects.filter(section_id=4)
+    # return HttpResponse(latest_products)
+    return render(request,'index.html', {'sliders' : sliders, 'latest': latest_products})
 
 def show_intro(request):
     return HttpResponse("This in intro")
