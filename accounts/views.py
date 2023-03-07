fcae009c45c4b1d47bd5dfa11af8c53faa19b500
abiraltamang <abiral.tamang816@gmail.com>
2023-03-07 14:51:42 +0545

    login and register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login(request):
+    return  HttpResponse("Hello")
+
+
+def register(request):
+    if request.method == 'POST':
+        return HttpResponse("Hello i am register")
+    return HttpResponse('Invalid Access')
--- django_app/urls.py
+++ django_app/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', include('contents.urls')),
     path('products/', include('products.urls') ),
+    path('accounts/', include('accounts.urls') ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
