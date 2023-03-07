f0df27c5421497c6e035bb99685844db3476b473
abiraltamang <abiral.tamang816@gmail.com>
2023-03-07 11:09:30 +0545

    accounts and models for shopping

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    qty = models.TextField()
+
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    qty = models.TextField()
+    address = models.TextField()
+    mobile = models.CharField(max_length=255)
+    price= models.FloatField()
+    discount = models.FloatField()
+    date = models.DateField()
+    status = models.CharField(max_length=255, 
+                              choices=[
+                                ('Requested','Requested'),
+                                ('Dispatched','Dispatched'),
+                                ('Delivered','Delivered'),
+                                ('Cancelled','Cancelled'),
+                                    ], default= ' Requested')
+
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    review = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to='reviews', null=True, blank=True)
+    rating = models.IntegerField()
+
+
+
